212d49cd0b6244947fd7625c156912d708537fd7
Xavier Magallanes <xmagalla@espol.edu.ec>
2020-11-14 20:28:02 -0500

    GUI FUNCIONANDO SIN MEDIA

--- Estructura/src/GUI/gui.java
+++ Estructura/src/GUI/gui.java
@@ -5,14 +5,15 @@
  */
 package GUI;
 
+import java.time.LocalDateTime;
+import javafx.application.Platform;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
-
 /**
  *
  * @author Xavier
@@ -20,18 +21,49 @@ import javafx.scene.media.MediaView;
 
 public class gui{
     private VBox root = new VBox();
-    private Label reloj = new Label();
-    private HBox principal = new HBox();
-    private Media video = new Media("direccion del video");
-    private MediaPlayer reproductor = new MediaPlayer(video);
-    private Label horarios = new Label("Horario de atención de Lunes a Viernes de 10 a 1 hs/ Sabados");
+    private Label reloj = new Label("");
+    Thread cl = new Thread(new Time(reloj));
+    private HBox p = new HBox();
+    private Label horarios = new Label("Horario de atención de Lunes a Viernes de 10 a 18 hs/ Sabados");
     private TableView turnos = new TableView();
-    private MediaView mv = new MediaView();
-    public void organizarinterfaz(){
-        principal.getChildren().addAll(mv, turnos);
-        root.getChildren().addAll(reloj, principal, horarios);
+    private Button opciones = new Button("OPCIONES");
+    
+    public gui(){
+        p.getChildren().addAll(opciones, turnos);
+        root.getChildren().addAll(reloj, p, horarios);
     }
     public VBox getRoot(){
         return root;
         }
+    private class Time implements Runnable {
+
+        private Label lbl;
+        LocalDateTime tp;
+
+        public Time(Label lbl) {
+            this.lbl = lbl;
+            lbl.setId("hora");
+        }
+
+        @Override
+        public void run() {
+            
+                try {
+                    tp = LocalDateTime.now();
+                    int hour = tp.getHour();
+                    int minute = tp.getMinute();
+                    int second = tp.getSecond();
+
+                    Platform.runLater(() -> {
+                        lbl.setText(hour+":"+minute+":"+second);
+                        
+                    });
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println(e.getMessage());
+                }
+            
+        }
+    
+}
 }
